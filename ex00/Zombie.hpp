fb719c1c89665f78599e0ba10fff6d05af8bbd80
Viking Okvist <vikingokvist@gmail.com>
2025-02-13 23:42:19 +0100

    ---

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+    private:
+        std::string name;
+        void    announce( void );
+    public:
+};
+
+
+#endif
