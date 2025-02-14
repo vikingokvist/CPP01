5107c8dc8fcfd5b13bfe0b144d9671f95c1512a1
Cristopher Tommasi <ctommasi@student.42madrid.com>
2025-02-14 14:38:13 +0100

    ex02----

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,1 +1,18 @@
-#include <io
+#include <iostream>
+
+int		main(void)
+{
+	std::string hiThisIsBrain = "HI THIS IS BRAIN";
+	std::string *stringPTR = &hiThisIsBrain;
+	std::string &stringREF = hiThisIsBrain;
+
+	std::cout << &hiThisIsBrain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << hiThisIsBrain << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return (0);
+}
